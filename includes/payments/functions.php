a71ab130e955c3ab0de15c8d392c09f1bbc32129
Kamal Ahmed <kamalacca@gmail.com>
2018-02-06 20:45:03 +0600

    added checkout related helper functions and other work in progress commit for saving a snaption

---
+++ includes/payments/functions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Directorist Payment Functions
+ *
+ * @package     Directorist
+ * @subpackage  Payment
+ * @copyright   Copyright (c) 2018, AazzTech
+ * @license     http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @since       3.0.0
+ */
+
+
+/*
+ * Placeholder functions to work as blue prints. These functions to be extended with features as we work on.*/
+/*IT WORKS LIKE AN INTERFACE*/
+
+/**
+ * @param string $status
+ * @return string
+ */
+function atbdp_get_payment_status($status = ''){
+    return $status;
+}
+
+/**
+ * @param $payment_id
+ * @param $status
+ */
+function atbdp_update_payment_status($payment_id, $status){
+
+}
+
+function atbdp_purchase_form_required_fields(){
+    return array();
+}
